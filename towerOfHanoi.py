92b829121fe4c361f549fb27853bd9149ba5ebde
Martin Gaens <djsushi123@gmail.com>
2016-07-27 17:29:58 +0200

    added script: hanoi

---
+++ towerOfHanoi.py
@@ -0,0 +1,13 @@
+def hanoi(n, source, helper, target):
+    if n > 0:
+        hanoi(n - 1, source, target, helper)
+        if source:
+            target.append(source.pop())
+        hanoi(n - 1, helper, source, target)
+        
+source = [4,3,2,1]
+target = []
+helper = []
+hanoi(len(source),source,helper,target)
+
+print source, helper, target
